ccc556b5288dd66fa65b3ab815632f4c12024ea1
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2015-04-28 12:16:28 +0100

    version bump 1.3 -> 1.4
    - does not cancel damage from cacti, allowing boat collection systems to work as intended
    - reformatted config.yml

--- src/main/java/com/cyclometh/bukkit/plugins/toughboats/BoatEventListener.java
+++ src/main/java/com/cyclometh/bukkit/plugins/toughboats/BoatEventListener.java
@@ -6,11 +6,14 @@
 
 package com.cyclometh.bukkit.plugins.toughboats;
 
+import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.entity.Player;
+import org.bukkit.event.vehicle.VehicleBlockCollisionEvent;
 import org.bukkit.event.vehicle.VehicleDestroyEvent;
+import org.bukkit.metadata.FixedMetadataValue;
 
 /*
  * Listens for and cancels any boat destruction events.
@@ -24,25 +27,44 @@ public class BoatEventListener implements Listener {
 	{
 		this.plugin=plugin;
 	}
-	
+
+    /**
+     * Checks for boats that collide with cacti. This is needed as CraftBukkit does not
+     * appear to pass cactus damage source/events to VehicleDestroyEvent.
+     */
+    @EventHandler
+    public void onVehicleCollide(VehicleBlockCollisionEvent event)
+    {
+        if (event.getVehicle().getType() == EntityType.BOAT)
+        if (event.getBlock().getType()   == Material.CACTUS)
+            event.getVehicle().setMetadata( "hitCactus", new FixedMetadataValue(plugin, true) );
+    }
+
 	/*
 	 * Listen for VehicleDestroyedEvent.
 	 */
 	@EventHandler
 	public void onVehicleDestroyed(VehicleDestroyEvent event){
-		
+
 		//if it's not a boat, we're not interested.
 		if (event.getVehicle().getType()!=EntityType.BOAT)
 			return;
+
 		//if it's already cancelled, we don't care.
 		if(event.isCancelled())
 			return;
+
 		//if damage is from fire or lava, we don't try to stop it.
 		//getFireTicks() will be -1 if the boat isn't on fire.
 		if(event.getVehicle().getFireTicks() != -1)
 			return;
-		
-		if (event.getAttacker() == null && !event.isCancelled())
+
+        // If damage is from cactus, don't stop it. This allows boat collection systems
+        // to work as intended. Relies on metadata set in onVehicleCollide
+        if ( event.getVehicle().hasMetadata("hitCactus") )
+            return;
+
+        if (event.getAttacker() == null)
 		{
 			event.setCancelled(true);
 			if(plugin.getConfig().getBoolean("debug", false))
@@ -52,7 +74,7 @@ public class BoatEventListener implements Listener {
 						(int)event.getVehicle().getLocation().getY(),
 						(int)event.getVehicle().getLocation().getZ()
 						));
-						
+
 		}
 		
 		
