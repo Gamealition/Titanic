998e9fbda2ef7a08ecd95f1a69624f701ce018d9
Roy Curtis <roy.adrian.curtis+github@gmail.com>
2016-05-20 23:31:04 +0100

    Simplification of code and project
    * Consolidated listener code to single class and handler
    * Removed Config class and yml; no config options to set
    * Rewrote README with features, bugs and dev notes

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
-A Bukkit plugin intended to make boats a little less like spun sugar and more like,
-well... a boat.
+Titanic is a Bukkit plugin for 1.9.4 and above. Its sole function is to try to fix [MC-91206]
+(https://bugs.mojang.com/browse/MC-91206), a regression where boats are sunk by entering sloped
+water, currents and waterfalls.
 
-Originally coded by [Cyclometh](https://github.com/Cyclometh/), updated to Spigot 1.8.3
-with changes by [Roy Curtis](https://github.com/RoyCurtis). Licensed under MIT.
+This plugin is not perfect. Since 1.9, boat movement whilst riding is handled by the client. The
+plugin has to fight against the client's movements by setting velocity. It is too difficult to
+emulate pre-1.9 behavior.
+
+Titanic used to be ToughBoats; a plugin originally coded by [Cyclometh]
+(https://github.com/Cyclometh). Before 1.9, ToughBoats fixed a variety of boat problems. It lives
+on in the `legacy` branch.
+
+# Behavior
+
+Currently, Titanic is able to push boats upwards if they:
+
+* Spawn or otherwise end up underwater
+* Attempt to enter a current or slope
+* Float down a current or stream
+* Enter a waterfall
+* Start sinking because of partial exposure to waterfall
+
+Titanic has trouble with:
+
+* Keeping players attached; if a boat has been underwater too long, the client will force a 
+  dismount. This cannot be solved by cancelling `VehicleExitEvent` as there is no reliable way to
+  tell when a player is intentionally dismounting.
+* Smooth rides; Titanic works by setting velocity on ridden boats. This interrupts momentum on the
+  client side, making movement jerky or unpredictable.
+* Waterfalls/elevators; sometimes boats may shoot too far up too fast, or shoot through the water
+    
+# Dev notes
+
+For anybody who wants to implement a similar fix, these are my experiences from developing Titanic:
+
+* When a boat is not ridden by a player, the server is in full control of its velocity and position.
+  This makes it easy to prevent sinking by just applying an upward velocity. The movement ends up
+  being smooth on the client-side.
+* Initially, I thought VehicleMoveEvent did not fire if ridden by player. Spigot used to handle it
+  only via PlayerMoveEvent, [but since fixed that](https://hub.spigotmc.org/jira/browse/SPIGOT-2043).
+* VehicleMoveEvent is not cancellable, so cancelling sinking packets is not an option
+* When a boat is ridden by a player, the client is almost in full control:
+  * It seems to ignore entity teleport packets for the ridden boat
+  * Using Entity.teleport will force a dismount; re-attaching in the same event is too jerky
+  * Server does not track velocity but does track position changes
+  * Client only responds to Entity.setVelocity
+  * Client will force a dismount if underwater too long; no way to tell if it's a manual dismount
 
 # Building
 
@@ -13,25 +55,10 @@ with changes by [Roy Curtis](https://github.com/RoyCurtis). Licensed under MIT.
 3. Inside the folder, execute `mvn clean package`
 4. Look in the new `target` folder for the built JAR file
 
-# Features
-
-* Prevents boats from breaking unless they...
-    * ...are hit by player
-    * ...are in fire or lava
-    * ...collide with a cactus
-* Keeps boat positions in sync between client and server (fixes [MC-2931]
-(https://bugs.mojang.com/browse/MC-2931))
-* Negates fall damage whilst on a boat (fixes [MC-881]
-(https://bugs.mojang.com/browse/MC-881))
-
-# Commands
-
-* `/toughboats reload` - Reloads the plugin and config.yml. Requires the permission
-**toughboats.reload** or OP.
-
 # Debugging
 
-ToughBoats makes use of `DEBUG` and `TRACE` logging levels for debugging. To enable these messages, append this line to the server's JVM arguments:
+Titanic makes use of `FINE` logging levels for debugging. To enable these messages, append this line
+to the server's JVM arguments:
 
 > `-Dlog4j.configurationFile=log4j.xml`
 
--- src/main/java/com/cyclometh/bukkit/plugins/toughboats/Titanic.java
+++ src/main/java/com/cyclometh/bukkit/plugins/toughboats/Titanic.java
@@ -1,20 +1,24 @@
 package com.cyclometh.bukkit.plugins.toughboats;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Boat;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import java.util.logging.Logger;
 
-/** Core class of the Titanic plugin. Handles config, registration and reload command */
-public class Titanic extends JavaPlugin
+/** Core class of the Titanic plugin. Handles listener creation and reload command */
+public class Titanic extends JavaPlugin implements Listener
 {
     static Logger LOGGER;
-
-    private BoatMoveListener   moveListener;
-    private BoatPacketListener packetListener;
 
     @Override
     public void onLoad()
@@ -25,43 +29,108 @@ public class Titanic extends JavaPlugin
     @Override
     public void onEnable()
     {
-        Config.init(this);
-
-        if ( Config.isNothingEnabled() )
-        {
-            LOGGER.warning("It appears no Titanic features are enabled.");
-            LOGGER.warning("Please check the config at `plugins/Titanic/config.yml`.");
-            return;
-        }
-
-        if (Config.unsinkableUnoccupiedBoats)
-            moveListener = new BoatMoveListener(this);
-
-        if (Config.unsinkableOccupiedBoats)
-            packetListener = new BoatPacketListener(this);
-
-        LOGGER.fine("Plugin fully enabled");
+        getServer().getPluginManager().registerEvents(this, this);
+        LOGGER.fine("Plugin fully enabled; listening for boat events");
     }
 
     @Override
     public void onDisable()
     {
-        HandlerList.unregisterAll(this);
-        Bukkit.getScheduler().cancelTasks(this);
-
-        moveListener   = null;
-        packetListener = null;
-
-        LOGGER.fine("Plugin fully disabled; all listeners and tasks unregistered");
+        HandlerList.unregisterAll((Listener) this);
+        LOGGER.fine("Plugin fully disabled; listener unregistered");
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
+    @EventHandler
+    @SuppressWarnings("deprecation")
+    /** This has to use deprecated .getData methods, as Bukkit has no API for getting water level */
+    public void onBoatMove(VehicleMoveEvent event)
     {
-        onDisable();
-        onEnable();
+        if (event.getVehicle().getType() != EntityType.BOAT)
+            return;
 
-        sender.sendMessage("[Titanic] Reloaded plugin and config.yml");
-        return true;
+        Entity   rider = event.getVehicle().getPassenger();
+        Boat     boat  = (Boat) event.getVehicle();
+        Location locTo = event.getTo();
+        Vector   vel   = boat.getVelocity();
+        Block    under = locTo.getBlock();                // New object created
+        Block    block = locTo.add(0, 0.6, 0).getBlock(); // WARN: Mutates locTo; new object created
+
+        // Handle empty boat. Non-player riders are considered "empty" as the server still handles
+        // their velocity and position updates
+        if (rider == null || rider.getType() != EntityType.PLAYER)
+        {
+            // Deal with boats that sink into blocks after running aground
+            if ( under.getType().isSolid() )
+                boat.teleport(locTo);
+            // Deal with boats that are inside water
+            else if ( block.isLiquid() )
+                boat.setVelocity( vel.setY(0.1) );
+            return;
+        }
+
+        // From this point, handle boat ridden by a player. This needs special treatment as client
+        // handles movement, so server must fight against it.
+
+        if ( !block.isLiquid() )
+            return;
+
+        Location locFrom   = event.getFrom();
+        Block    above     = block.getRelative(BlockFace.UP);
+        byte     blockData = block.getData();
+        byte     aboveData = above.getData();
+
+        // Using doubles instead of making a new vector; avoids generating garbage per tick
+        double velX = locTo.getX() - locFrom.getX();
+        double velY = locTo.getY() - locFrom.getY() - 0.6;
+        double velZ = locTo.getZ() - locFrom.getZ();
+
+        // Whilst ridden by a player, server's boat velocity is always 0. Thus, preparing own
+        // recorded velocity into the vector for later possible use
+        vel.setX(velX).setZ(velZ);
+
+        /**
+         * Boat and block position findings, from logging (and lots of trial & error):
+         *
+         * * If boat's block has data 0, then boat is underwater or is sinking
+         *   * If above block is air, then boat is in shallow water
+         *   * If above block is water with data 0, then boat is deep underwater
+         *   * If above block is water with data 1 and 15, then boat is under a slope/waterfall
+         * * If boat's block has data between 1 and 7, then boat is riding a slope
+         *   * Can be assumed that above block is AIR
+         * * If boat's block has data between 8 and 15, then boat is inside waterfall
+         *   * If above block has data between 0 and 7, then that is waterfall terminus
+         *   * If above block has data between 8 and 15, then waterfall is continuing higher
+         */
+
+        // Deal with underwater/sinking boat
+        if (blockData == 0)
+        {
+            // Deal with boats inside slope
+            if (aboveData >= 1)
+            {
+                // Compensate based on how thick the slope is, with maximum
+                velY = ( (double) (8 - blockData) ) / 15;
+                velY = Math.min(velY, 0.4);
+            }
+            // Deal with boats that are just sinking vertically
+            else
+                // Compensate based on currently recorded sinking velocity
+                velY = Math.abs(velY) + 0.1;
+        }
+
+        // Deal with boats riding top-most slope. Too jumpy if all slopes handled
+        else if (blockData == 1)
+            velY = 0.175;
+
+        // Discontinue if boat is in intermediate slopes
+        else if (blockData > 1 && blockData < 8)
+            return;
+
+        // Deal with boats inside a waterfall
+        else if (blockData >= 8)
+            velY = (aboveData >= 8 ? 1 : 0.5);
+
+        // Apply velocity adjustment; interrupts client-side momentum with unpredictable results
+        boat.setVelocity( vel.setY(velY) );
     }
 }
